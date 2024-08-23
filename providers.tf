d8e4fe3c601b86bcab0ac0939cbe2efacaf0dd2f
Bob Cauthen <89991256+CBCauthen@users.noreply.github.com>
2024-08-23 16:20:33 -0400

    moved block

--- providers.tf
+++ providers.tf
@@ -3,3 +3,11 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-a"
 }
+
+provider "google" {
+ alias = "impersonation"
+ scopes = [
+   "https://www.googleapis.com/auth/cloud-platform",
+   "https://www.googleapis.com/auth/userinfo.email",
+ ]
+}
