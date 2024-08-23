45dafe3818ffc854554599999437de2e36e4dd71
Bob Cauthen <89991256+CBCauthen@users.noreply.github.com>
2024-08-23 16:22:24 -0400

    moved blocks and rm tf.tfvars file

--- providers.tf
+++ providers.tf
@@ -11,3 +11,16 @@ provider "google" {
    "https://www.googleapis.com/auth/userinfo.email",
  ]
 }
+
+data "google_service_account_access_token" "default" {
+ provider               	= google.impersonation
+ target_service_account 	= terraform_service_account
+ scopes                 	= ["userinfo-email", "cloud-platform"]
+ lifetime               	= "1200s"
+}
+
+provider "google" {
+ project 		= cauthen_sandbox
+ access_token	= data.google_service_account_access_token.default.access_token
+ request_timeout 	= "60s"
+}
