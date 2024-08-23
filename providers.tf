46cf6f2970b661fe5aa5135b93fe13c8f55505b3
Bob Cauthen <89991256+CBCauthen@users.noreply.github.com>
2024-08-23 16:25:38 -0400

    added local scope

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ provider "google" {
 
 data "google_service_account_access_token" "default" {
  provider               	= google.impersonation
- target_service_account 	= terraform_service_account
+ target_service_account 	= local.terraform_service_account
  scopes                 	= ["userinfo-email", "cloud-platform"]
  lifetime               	= "1200s"
 }
